7e4ae0617d09f1f1a1677687bb5cbe530e5e0959
Amale Dkhissi <amdkhissi@gmail.com>
2025-04-10 17:19:54 +0200

    feat: add fonts

---
+++ src/app/fonts.tsx
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+export const gagalin = localFont({
+  src: "./fonts/GagalinRegular.otf",
+  display: "swap",
+  variable: "--font-gagalin",
+});
+
+export const dreamingOutloud = localFont({
+  src: "./fonts/DreamingOutloudAllcapsRegular.otf",
+  display: "swap",
+  variable: "--font-dreaming",
+});
